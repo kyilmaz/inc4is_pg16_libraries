929c381941521ecf345ff994a21e708dd43270cd
kyilmaz <kyilmaz@users.noreply.github.com>
2024-08-06 19:27:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# inc4is_pg16_libraries
+ postgresql 16.3 libraries copy for doing code analysis
---
+++ event2/event-config.h
@@ -0,0 +1,9 @@
+#include <bits/wordsize.h>
+
+#if __WORDSIZE == 32
+#include <event2/event-config-32.h>
+#elif __WORDSIZE == 64
+#include <event2/event-config-64.h>
+#else
+#error "Unknown word size"
+#endif
